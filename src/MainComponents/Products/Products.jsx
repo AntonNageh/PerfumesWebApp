d5a608805d6914eec93ae4898eb2afc4437f3d61
AntonNageh <92064286+AntonNageh@users.noreply.github.com>
2023-09-10 23:34:59 +0300

    ResponsivenessAdjs

--- src/MainComponents/Products/Products.jsx
+++ src/MainComponents/Products/Products.jsx
@@ -10,7 +10,7 @@ const Products = ({name,price,src}) => {
       <button onClick={()=>navigate(`/product/${uniqueId}`)}>
         <img src={src} className="absolute object-contain  w-full h-full -z-10"></img>
         <div className='mt-[55vw] md:mt-[60%] lg:mt-[17vw] xl:mt-[20vw] flex text-left'>
-          <div className="lg:ml-[25%] lg:mt-[3vw]">
+          <div className="lg:ml-[30%] lg:mt-[-4vw]">
             <h1 className='ml-5 opacity-60'>Perfume</h1>
             <h1 className='text-[4.5vw]  lg:text-[4vh]  mt-1 font-bold ml-5'>{name}</h1>
           </div>
